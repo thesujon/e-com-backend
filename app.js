43bce42f896650fae6e4ff0138fec05f42da139e
Md Motiur Rahman <thesujon@gmail.com>
2023-09-07 09:28:31 +0600

    Project First Deployment

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 const error = require('./middlewares/erros');
+const compression = require('compression');
 
 require('./middlewares')(app);
 require('./middlewares/routes')(app);
 
+app.use(compression());
 app.use(error);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-require('dotenv/config');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/bohubrihi-ecommerce')
-//mongoose.connect(process.env.MONGODB_URL_LOCAL)
+
+//mongoose.connect('mongodb://localhost:27017/bohubrihi-ecommerce')
+const DB = process.env.MONGODB_URL_SERVER.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+})
+
     .then(() => console.log("Connected to MongoDB!"))
     .catch(err => console.error("MongoDB Connection Failed!"));
 
